024ab356ba72d50d445f5859f8caca529020151a
ya7ya99 <yahyakhaldy9@gmail.com>
2023-11-17 06:22:42 -0800

    updeat

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,50 @@
-#include "sort.h"
+#include	"sort.h"
 
 /**
- * swap - Swaps two nodes in a doubly linked list
- * @a: First node to swap
- * @b: Second node to swap
- */
-void swap(listint_t *a, listint_t *b)
+	*	swap	-	Swaps	two	nodes	in	a	doubly	linked	list
+	*	@a:	First	node	to	swap
+	*	@b:	Second	node	to	swap
+	*/
+void	swap(listint_t	*a,	listint_t	*b)
 {
-	if (a->prev)
-		a->prev->next = b;
-	if (b->next)
-		b->next->prev = a;
-	a->next = b->next;
-	b->prev = a->prev;
-	b->next = a;
-	a->prev = b;
+	if	(a->prev)
+	a->prev->next	=	b;
+	if	(b->next)
+	b->next->prev	=	a;
+
+	a->next	=	b->next;
+	b->prev	=	a->prev;
+	b->next	=	a;
+	a->prev	=	b;
 }
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- * in ascending order using the Insertion sort algorithm
- * @list: Pointer to the head of the linked list
- */
-void insertion_sort_list(listint_t **list)
+	*	insertion_sort_list	-	Sorts	a	doubly	linked	list	of	integers
+	*	in	ascending	order	using	the	Insertion	sort	algorithm
+	*	@list:	Pointer	to	the	head	of	the	linked	list
+	*/
+void	insertion_sort_list(listint_t	**list)
 {
-	listint_t *current, *temp;
+	listint_t	*current,	*temp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+	if	(list	==	NULL	||	*list	==	NULL	||	(*list)->next	==	NULL)
+	return;
 
-	current = (*list)->next;
-	while (current != NULL)
+
+	current	=	(*list)->next;
+
+	while	(current	!=	NULL)
 	{
-		temp = current;
-		while (temp->prev != NULL && temp->n < temp->prev->n)
-		{
-			swap(temp->prev, temp);
-			if (temp->prev == NULL)
-			{
-				*list = temp;
-				print_list(*list);
-			}
-		}
-		current = current->next;
+	temp	=	current;
+
+	while	(temp->prev	!=	NULL	&&	temp->n	<	temp->prev->n)
+	{
+	swap(temp->prev,	temp);
+	if	(temp->prev	==	NULL)
+	*list	=	temp;
+	print_list(*list);
+	}
+
+	current	=	current->next;
 	}
 }
