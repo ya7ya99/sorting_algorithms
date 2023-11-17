bc3769ef555fcf94e649369f6b4a7875379404d5
ya7ya99 <yahyakhaldy9@gmail.com>
2023-11-17 06:13:05 -0800

    updeat

--- sort.h
+++ sort.h
@@ -20,7 +20,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-
+void swap(listint_t *a, listint_t *b);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
