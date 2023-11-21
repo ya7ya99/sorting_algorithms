5e09449767b4c6ea5ad6693aaca98eb53c19f26f
ya7ya99 <yahyakhaldy9@gmail.com>
2023-11-21 02:22:57 -0800

    updeat

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -10,16 +10,16 @@
  */
 int getMax(int *array, size_t size)
 {
-    int max = array[0];
-    size_t i;
+	int max = array[0];
+	size_t i;
 
-    for (i = 1; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
 
-    return max;
+	return max;
 }
 
 /**
@@ -30,30 +30,30 @@ int getMax(int *array, size_t size)
  */
 void countingSort(int *array, size_t size, int exp)
 {
-    int *output = malloc(sizeof(int) * size);
-    int count[10] = {0};
-    size_t i;
+	int *output = malloc(sizeof(int) * size);
+	int count[10] = {0};
+	size_t i;
 
-    if (!output)
-        return;
+	if (!output)
+		return;
 
-    for (i = 0; i < size; i++)
-        count[(array[i] / exp) % 10]++;
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
 
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
 
-    for (i = size - 1; i < size; i--)
-    {
-        output[count[(array[i] / exp) % 10] - 1] = array[i];
-        count[(array[i] / exp) % 10]--;
-    }
+	for (i = size - 1; i < size; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
 
-    for (i = 0; i < size; i++)
-        array[i] = output[i];
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
 
-    print_array(array, size);
-    free(output);
+	print_array(array, size);
+	free(output);
 }
 
 /**
@@ -64,14 +64,13 @@ void countingSort(int *array, size_t size, int exp)
  */
 void radix_sort(int *array, size_t size)
 {
-    int max, exp;
+	int max, exp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    max = getMax(array, size);
+	max = getMax(array, size);
 
-    for (exp = 1; max / exp > 0; exp *= 10)
-        countingSort(array, size, exp);
+	for (exp = 1; max / exp > 0; exp *= 10)
+		countingSort(array, size, exp);
 }
-
