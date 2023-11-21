877b5af6c3b61c9568a47a568bba0608f9ae20d8
ya7ya99 <yahyakhaldy9@gmail.com>
2023-11-21 02:17:06 -0800

    updeat

---
+++ 105-radix_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * getMax - Gets the maximum value in an array
+ * @array: The array
+ * @size: Size of the array
+ * Return: The maximum value
+ */
+int getMax(int *array, size_t size)
+{
+    int max = array[0];
+    size_t i;
+
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+    }
+
+    return max;
+}
+
+/**
+ * countingSort - Sorts an array using counting sort based on significant place
+ * @array: The array to be sorted
+ * @size: Size of the array
+ * @exp: The current significant place (1, 10, 100, ...)
+ */
+void countingSort(int *array, size_t size, int exp)
+{
+    int *output = malloc(sizeof(int) * size);
+    int count[10] = {0};
+    size_t i;
+
+    if (!output)
+        return;
+
+    for (i = 0; i < size; i++)
+        count[(array[i] / exp) % 10]++;
+
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (i = size - 1; i < size; i--)
+    {
+        output[count[(array[i] / exp) % 10] - 1] = array[i];
+        count[(array[i] / exp) % 10]--;
+    }
+
+    for (i = 0; i < size; i++)
+        array[i] = output[i];
+
+    print_array(array, size);
+    free(output);
+}
+
+/**
+ * radix_sort - Sorts an array of integers in ascending order
+ *              using the Radix sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+    int max, exp;
+
+    if (!array || size < 2)
+        return;
+
+    max = getMax(array, size);
+
+    for (exp = 1; max / exp > 0; exp *= 10)
+        countingSort(array, size, exp);
+}
+
